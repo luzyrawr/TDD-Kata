e3d4c839006b11682512b3107f906db3e7f03b99
luzgalindo <luz.galindo@ipointsystems.com>
2021-02-25 17:01:06 -0600

    Pasa prueba para ""

--- StringCalculator/StringCalculatorKata/StringCalculator.cs
+++ StringCalculator/StringCalculatorKata/StringCalculator.cs
@@ -6,6 +6,11 @@ namespace StringCalculatorKata
     {
         public string Add(string numbers)
         {
+            var arrNumbers = numbers.Split(",");
+
+            if (arrNumbers[0] == "")
+                return "0";
+
             return string.Empty;
         }
     }
