8a533db5f7461c734d694317b20ff858a721796c
luzgalindo <luz.galindo@ipointsystems.com>
2021-02-25 16:59:12 -0600

    No error de compilacion

---
+++ StringCalculator/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public string Add(string numbers)
+        {
+            return string.Empty;
+        }
+    }
+}
