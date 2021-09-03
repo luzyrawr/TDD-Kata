6b92ea3bf7ea8b34c878ac07c3ff9b997d0e7785
luz.galindo <luz.galindo@ipointsystems.com>
2021-09-03 09:52:31 -0500

    Fibonacci Kata

---
+++ FibonacciNumbersKata.Tests/FibonacciTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using FibonacciNumbersKata;
+
+namespace FibonacciNumbersKata.Tests
+{
+    /* Sequene of Fibonacci Numbers
+     * 1, 1, 2, 3, 5, 8, 13, 21, 34
+     * or
+     * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+     */
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 3)]
+        [TestCase(5, 5)]
+        [TestCase(6, 8)]
+        public void TestFibonacci(int index, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Fibonacci.GetFibonacci(index));
+        }
+    }
+}
---
+++ FibonacciNumbersKata/Fibonacci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciNumbersKata
+{
+    public class Fibonacci
+    {
+        public static int GetFibonacci(int index)
+        {
+            if (index == 0) return 0;
+            if (index == 1) return 1;
+            return GetFibonacci(index - 2) + GetFibonacci(index - 1);
+        }
+    }
+}
