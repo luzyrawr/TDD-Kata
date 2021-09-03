d868bbe1088dd618a4e4a4e8274ce2b1bf195f3d
luz.galindo <luz.galindo@ipointsystems.com>
2021-09-03 10:20:04 -0500

    New FizzBuzz Kata and projects

---
+++ FizzBuzzKata.Tests/FizzBuzzTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace FizzBuzzKata.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {   
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
